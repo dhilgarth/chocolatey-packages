dd2880796a911ee7f35b13b7644a8a10efc1de94
Daniel Hilgarth <d.hilgarth@fire-development.com>
2015-02-26 21:47:00 +0100

    Add support for old PowerShell versions of Chocolatey

--- procexp/tools/chocolateyinstall.ps1
+++ procexp/tools/chocolateyinstall.ps1
@@ -8,4 +8,13 @@ if(!(Test-Path $shortcutPath)) {
     md $shortcutPath
 }
 
-Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath (Join-Path $installDir "procexp.exe")
+$targetPath = Join-Path $installDir "procexp.exe"
+if(Get-Command "Install-ChocolateyShortcut" -ErrorAction SilentlyContinue) { # New, compiled Choco
+    Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath $targetPath
+}
+else { # PowerShell Choco
+    $shell = New-Object -comObject WScript.Shell
+    $shortcut = $shell.CreateShortcut($shortcutFilePath)
+    $shortcut.TargetPath = $targetPath
+    $shortcut.Save()
+}
