47a4e467f1a476b56ce785c7185415bf6bb44d13
Daniel Hilgarth <d.hilgarth@fire-development.com>
2015-02-26 16:43:55 +0100

    Add shortcut in Sysinternals program group

--- procmon/tools/chocolateyinstall.ps1
+++ procmon/tools/chocolateyinstall.ps1
@@ -1,1 +1,11 @@
-Install-ChocolateyZipPackage 'procmon' 'http://download.sysinternals.com/files/ProcessMonitor.zip' "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$installDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Install-ChocolateyZipPackage 'procmon' 'http://download.sysinternals.com/files/ProcessMonitor.zip' $installDir
+
+$programs = [environment]::GetFolderPath([environment+specialfolder]::Programs)
+$shortcutPath = Join-Path "$programs" "Sysinternals"
+$shortcutFilePath = Join-Path $shortcutPath "Process Monitor.lnk"
+if(!(Test-Path $shortcutPath)) {
+    md $shortcutPath
+}
+
+Install-ChocolateyShortcut -shortcutFilePath $shortcutFilePath -targetPath (Join-Path $installDir "procmon.exe")
