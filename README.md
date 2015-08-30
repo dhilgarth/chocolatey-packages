eb8d3c5644da9fbea81ed70f4f8b1e7f3b0cca35
Daniel Hilgarth <d.hilgarth@fire-development.com>
2015-08-30 22:01:55 +0200

    Add first version of GitFlow-AVH

--- README.md
+++ README.md
@@ -8,6 +8,6 @@ This repository hosts the code of the Chocolatey packages I am a maintainer of. 
     - Sysinternals Process Monitor (procmon)
     - TechSmith SnagIt (snagit)
 - Manual Packages
-    - None so far
+    - git-flow AVH Edition (gitflow-avh)
 
 If you don't know Chocolatey, check it out on [chocolatey.org](http://chocolatey.org)! It is a Machine Package Manager, somewhat like apt-get, but built with Windows in mind.
---
+++ manual/gitflow-avh/tools/chocolateyInstall.ps1
@@ -0,0 +1,50 @@
+﻿$giturl = "https://github.com/petervanderdoes/gitflow.git"
+
+# Refresh environment from registry
+Update-SessionEnvironment
+
+# Detect Git
+$apps = @(Show-AppUninstallInfo -match "Git version [0-9\.]+(-preview\d*)?")
+
+if ($apps.Length -eq 0)
+{
+    throw "Could not detect a valid Git installation"
+}
+
+$app = $apps[0]
+$gitDir = $app["InstallLocation"]
+
+if (-not (Test-Path "$gitDir"))
+{
+    throw "Local Git installation is detected, but directories are not accessible or have been removed"
+}
+
+# Now clone the repository. Git executable could not be in PATH
+# so we are using absolute filenames. Everything must be executed
+# with elevated privileges
+#
+$gitflowDir = Join-Path "$gitDir" "gitflow"
+$exeGit = Join-Path "$gitDir" "cmd\git.exe"
+$exeInstallGitFlow = Join-Path "$gitflowDir" "contrib\msysgit-install.cmd"
+$gitBin = Join-Path "$gitDir" "usr"
+$gitBin64 = Join-Path "$gitDir" "mingw64"
+
+if (-not (Test-Path $gitBin))
+{
+    $gitBin = Join-Path "$gitDir" "mingw"
+}
+
+if (Test-Path $(Join-Path "$gitDir" "git-flow"))
+{
+    Write-Host "`nFound existing Git-Flow. Removing...`n"  -foregroundcolor yellow
+    Start-ChocolateyProcessAsAdmin "Get-ChildItem -path '$gitDir' -include 'git-flow*','gitflow-*','gitflow*' -recurse -force | Remove-Item -recurse -force" -minimized
+}
+
+Write-Host "`nGit-Flow: Cloning repository from GitHub and installing Git-Flow ...`n"  -foregroundcolor yellow
+
+Start-ChocolateyProcessAsAdmin "/c `"`"$exeGit`" clone --recursive `"$giturl`" `"$gitflowDir`"`"" -exe "$env:comspec" -minimized
+Start-ChocolateyProcessAsAdmin "/c `"`"$exeInstallGitFlow`" `"$gitBin`"`"" -exe "$env:comspec" -minimized
+if (Test-Path $gitBin64)
+{
+    Start-ChocolateyProcessAsAdmin "/c `"`"$exeInstallGitFlow`" `"$gitBin64`"`"" -exe "$env:comspec" -minimized
+}
