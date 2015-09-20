d5aca8424f622e25ae21f8c225e3064edf6ccc5b
Daniel Hilgarth <d.hilgarth@fire-development.com>
2015-09-20 13:19:38 +0200

    pdffactorypro-workstation: Use extensions to parse parameters

--- automatic/pdffactorypro-workstation/tools/chocolateyinstall.ps1
+++ automatic/pdffactorypro-workstation/tools/chocolateyinstall.ps1
@@ -1,34 +1,15 @@
+$toolsPath = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+. "$toolsPath\extensions.ps1"
+
 $packageName = '{{PackageName}}' # arbitrary name for the package, used in messages
 $installerType = 'EXE'
 $url = '{{DownloadUrl}}'
 $silentArgs = '/quiet /autodelete'
 $validExitCodes = @(0) 
 
-$packageParameters = $env:chocolateyPackageParameters;
+$arguments = (ParseParameters $env:chocolateyPackageParameters)
 
 $languageMap = @{ "zh-CN" = "0804"; "zh-TW" = "0404"; "cs" = "0405"; "da" = "0406"; "nl" = "0413"; "en" = "0409"; "fr" = "040c"; "de" = "0407"; "it" = "0410"; "ja" = "0411"; "pl" = "0415"; "pt" = "0416"; "ru" = "0419"; "sk" = "041b"; "es" = "0c0a"; "sv" = "041d"; }
-
-$arguments = @{};
-
-if ($packageParameters) {
-    $match_pattern = "\/(?<option>([a-zA-Z]+)):([`"'])?(?<value>([a-zA-Z0-9- _\\:\.]+))([`"'])?|\/(?<option>([a-zA-Z]+))"  
-    #"
-    $optionName = 'option'
-    $valueName = 'value'
-    
-    if ($packageParameters -match $match_pattern ){
-        $results = $packageParameters | Select-String $match_pattern -AllMatches
-        $results.matches | % {
-          $arguments.Add(
-              $_.Groups[$optionName].Value.Trim(),
-              $_.Groups[$valueName].Value.Trim())
-      }
-    }
-    else
-    {
-      throw "Package Parameters were found but were invalid (REGEX Failure)"
-    }
-}
 
 $chocTempDir = Join-Path (Get-Item $env:TEMP).FullName "chocolatey"
 $tempDir = Join-Path $chocTempDir "$packageName"
