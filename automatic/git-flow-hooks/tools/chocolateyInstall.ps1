40214c96f82f6916870878cdfd74e0fdbe9e7e1a
Daniel Hilgarth <d.hilgarth@fire-development.com>
2015-09-21 15:03:33 +0200

    Change git-flow-hooks to automatic package
    Automatic package
    Fix package ID
    Remove creation of global config file
    Add option to decide whether or not to install globally

---
+++ automatic/git-flow-hooks/tools/chocolateyInstall.ps1
@@ -0,0 +1,26 @@
+﻿$toolsPath = (Split-Path -parent $MyInvocation.MyCommand.Definition)
+. "$toolsPath\extensions.ps1"
+
+$giturl = "https://github.com/jaspernbrouwer/git-flow-hooks.git"
+
+# Refresh environment from registry
+Update-SessionEnvironment
+
+$exeGit = Get-FullAppPath "Git version [0-9\.]+(-preview\d*)?" "cmd" "git.exe"
+
+$installDir = Join-Path (Split-Path -parent $toolsPath) "repository"
+
+if((Test-Path $installDir) -and (Test-Path (Join-Path $installDir ".git"))) {
+    cd $installDir
+    & "$exeGit" fetch --all --tags
+}
+else {
+    & "$exeGit" clone --recursive "$giturl" "$installDir" 2>&1 | write-host
+    cd $installDir
+}
+& "$exeGit" checkout tags/v{{PackageVersion}} 2>&1 | write-host
+
+$arguments = (ParseParameters $env:chocolateyPackageParameters)
+if(($arguments.ContainsKey("global")) -and ($arguments["global"] -eq "true")) {
+    & "$exeGit" config --global --replace gitflow.path.hooks $installDir
+}
